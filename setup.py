0896959eebf345363fc5ee42377076211a2b3e5d
kevin <kevinmchan@live.com>
2020-11-15 16:23:16 -0500

    Initial cookiecutter commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solutions to reinforcement learning problems',
+    author='Kevin Chan',
+    license='MIT',
+)
